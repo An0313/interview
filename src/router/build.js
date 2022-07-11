35976764c38b17a305fd1c9ddf9affc0dc74d416
anhengguang <anhengguang@kaikeba.com>
2022-07-11 16:01:41 +0800

    sass

---
+++ src/router/build.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const {pages, appConfig} = require('./index')
+
+const pagesJsonPath = path.resolve(__dirname, '../pages.json')
+const pagesObjPath = path.resolve(__dirname, '../constants/pages.ts')
+
+fs.writeFile(pagesJsonPath, JSON.stringify(appConfig, null, '  '), e => {
+  if (e) console.error(e)
+  else console.log('pages.json 配置成功')
+})
+
+fs.writeFile(pagesObjPath, 'export default ' + JSON.stringify(pages, null, '  '), e => {
+  if (e) console.error(e)
+  else console.log('pages.ts 配置成功')
+})
