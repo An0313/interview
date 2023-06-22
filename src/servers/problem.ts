a2d37cee45ffc4f9a6c3cd5eeebef54ff05b47be
An0313 <805024102@qq.com>
2023-06-22 19:10:08 +0800

    修改版本号 修改面试题请求地址

--- src/servers/problem.ts
+++ src/servers/problem.ts
@@ -4,5 +4,5 @@ import Request from "@/utils/request";
  * 获取面试题列表
  */
 export const getProblem = (): IProblem.GET_PROBLEM_DATA => Request({
-    url: 'http://qn-interview.an1688.top/problemData.json'
+    url: 'https://mp-66fedf64-a9d9-4aa2-b71e-15bab6b06995.cdn.bspapp.com/problemData.json'
 })
