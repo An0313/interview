d3af5e8cd5585aec52f6a107b19ca62304175147
An0313 <805024102@qq.com>
2025-01-23 10:28:25 +0800

    处理文件

--- src/const/pages.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  "home": "/pages/index/index",
-  "problemList": "/pages/problemList/index",
-  "problemDetail": "/pages/problemDetail/index",
-  "answerDetail": "/pages/answerDetail/index"
-}
